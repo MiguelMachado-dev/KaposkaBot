b331cd3196e908fb26257fd5be555fa61c456fdc
Miguel Machado <miiguellmp@gmail.com>
2020-07-17 09:59:39 -0300

    fixing meow command

--- commands/funny/meow.js
+++ commands/funny/meow.js
@@ -1,28 +1,26 @@
 const snek = require("snekfetch");
-const APIcat = "http://aws.random.cat/meow";
+const APIcat = "https://api.thecatapi.com/v1/images/search?size=full";
 
 module.exports = {
   name: "meow",
   category: "funny",
-  description: "Send a random dog media.",
+  description: "Send a random cat media.",
   run: async (client, message, args) => {
     const msg = await message.channel.send("> Indo pegar a câmera...");
 
-    const file = (await snek.get(APIcat)).body.file;
-    if (!file)
+    const response = (await snek.get(APIcat)).text; // Getting response
+    const responseJSON = JSON.parse(response); // Parsing into JSON
+    const url = responseJSON[0].url; // Getting url
+
+    if (!response)
       return message.channel.send(
         "É, não deu... Foi rápido de mais! Tente de novo ;("
       );
 
     await message.channel.send({
-      files: [
-        {
-          attachment: file,
-          name: file.split("/").pop()
-        }
-      ]
+      files: [url],
     });
 
     msg.delete();
-  }
+  },
 };
